8b4f4e6789cc7d513a4666a1bd0557b8dfb7c3b1
Your Name <you@example.com>
2021-11-27 14:31:25 -0700

    changes to index files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-function myFunction() {
-    document.getElementById("menu").innerHTML ="Data 📁, Form 🖥";
-  }
+
+var taskIdCounter = 0;
+
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
+/*$(document).ready(function(){
+  $("button").click(function(){
+    $("p").hide();
+  });
+});*/
+
+$(document).ready(function () {
+  $("#buttonContainer").dxButton({
+      text: "+",
+      onClick: function () {
+        $("[href]").fadeIn();
+      }})
+    });
+
+    $(".control_save").click(function () {
+          $("#client-form").saveForm();
+          return false;
+      });
+
+      $(".control_restore").click(function () {
+          $("#client-form").restoreForm();
+          return false;
+      });
