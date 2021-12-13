c0d9ba22ad059b7ffce3d656fe8244df548dcf43
vmcguire@unitedlocating.com+1-at-530555730387 <vmcguire@unitedlocating.com>
2021-12-13 11:09:48 -0700

    app added

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const app = express();
+const path = require('path');
+var app = express();
 
+app.use(express.static(path.join(__dirname, './')));
 
-/*app.use(express.static('public'));*/
-/*app.use(express.static(path.join(__dirname, './')));*/
+// assign port to PORT variable
+const PORT = 3001;
 
-
-
-/*app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));*/
- 
-app.listen(3001, () => {
-    console.log(`Server running at port 3001!`);
-  });
+// spin up server and console.log PORT
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}`);
+});
